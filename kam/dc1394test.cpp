9101b614147842ab4eb37efb1daa94f8d4690b2a
kroylar <kroylar@2674a16e-38bf-880a-777f-f6c340d2ef8c>
2010-10-05 17:49:13 +0000

    preliminary face detection

--- kam/dc1394test.cpp
+++ kam/dc1394test.cpp
@@ -1,3 +1,4 @@
+#include "opencv/cv.h"
 #include "opencv/highgui.h"
 #include "dc1394/dc1394.h"
 #include <iostream>
@@ -5,7 +6,35 @@
 #include <time.h>
 using namespace std;
 
+// Create memory for calculations
+static CvMemStorage* storage = 0;
+
+// Create a new Haar classifier
+static CvHaarClassifierCascade* cascade = 0;
+
+// Function prototype for detecting and drawing an object from an image
+void detect_and_draw( IplImage* image, int do_pyramids );
+
+// Create a string that contains the cascade name
+const char* cascade_name =
+    "haarcascade_frontalface_alt.xml";
+/*    "haarcascade_profileface.xml";*/
+
 int main( int argc, char** argv ) {
+
+    // Load the HaarClassifierCascade
+    cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
+
+    // Check whether the cascade has loaded successfully. Else report and error and quit
+    if( !cascade )
+    {
+        fprintf( stderr, "ERROR: Could not load classifier cascade\n" );
+        return -1;
+    }
+
+    // Allocate the memory storage
+    storage = cvCreateMemStorage(0);
+
     // firewire stuff
     // clear DMA ring buffer stop ISO transmission
     dc1394_t* dcenv = dc1394_new();
@@ -21,7 +50,8 @@ int main( int argc, char** argv ) {
     }
 
     dc1394_camera_free_list(camlist[0]);
-    
+
+    // TODO: only stop if its been started    
     dc1394_capture_stop(camera[0]);
     dc1394_capture_stop(camera[1]);
     
@@ -77,6 +107,7 @@ int main( int argc, char** argv ) {
 
     while(1) {
         frame1 = cvQueryFrame( capture1 );
+        detect_and_draw(frame1, 1);
         frame2 = cvQueryFrame( capture2 );
         if( !frame1 || !frame2 ) break;
         //cvShowImage( "Wide FOV", frame1 );
@@ -87,7 +118,7 @@ int main( int argc, char** argv ) {
         cvCopy(frame2, frame3);
         cvResetImageROI(frame3);
         cvShowImage("side-by-side", frame3);
-        char c = cvWaitKey(33);
+        char c = cvWaitKey(3);
         if (c == 27) {
             break;
         }
@@ -113,4 +144,91 @@ int main( int argc, char** argv ) {
     dc1394_camera_free(camera[1]);
     dc1394_free(dcenv);
 }
+
+void detect_and_draw( IplImage* image,
+                              int do_pyramids )
+{
+    IplImage* small_image = image;
+    CvMemStorage* storage = cvCreateMemStorage(0);
+    CvSeq* faces;
+    int i, scale = 1;
+
+    /* if the flag is specified, down-scale the input image to get a
+       performance boost w/o loosing quality (perhaps) */
+    if( do_pyramids )
+    {
+        small_image = cvCreateImage( cvSize(image->width/2,image->height/2), IPL_DEPTH_8U, 3 );
+        cvPyrDown( image, small_image, CV_GAUSSIAN_5x5 );
+        scale = 2;
+    }
+
+    /* use the fastest variant */
+    faces = cvHaarDetectObjects( small_image, cascade, storage, 1.2, 2, CV_HAAR_DO_CANNY_PRUNING );
+
+    /* draw all the rectangles */
+    for( i = 0; i < faces->total; i++ )
+    {
+        /* extract the rectanlges only */
+        CvRect face_rect = *(CvRect*)cvGetSeqElem( faces, i );
+        cvRectangle( image, cvPoint(face_rect.x*scale,face_rect.y*scale),
+                     cvPoint((face_rect.x+face_rect.width)*scale,
+                             (face_rect.y+face_rect.height)*scale),
+                     CV_RGB(255,0,0), 3 );
+    }
+
+    if( small_image != image )
+        cvReleaseImage( &small_image );
+    cvReleaseMemStorage( &storage );
+}
+
+
+/*
+void detect_and_draw( IplImage* img )
+{
+    int scale = 1.5;
+
+    // Create a new image based on the input image
+    IplImage* temp = cvCreateImage( cvSize(img->width/scale,img->height/scale), 8, 3 );
+
+    // Create two points to represent the face locations
+    CvPoint pt1, pt2;
+    int i;
+
+    // Clear the memory storage which was used before
+    cvClearMemStorage( storage );
+
+    // Find whether the cascade is loaded, to find the faces. If yes, then:
+    if( cascade )
+    {
+
+        // There can be more than one face in an image. So create a growable sequence of faces.
+        // Detect the objects and store them in the sequence
+        CvSeq* faces = cvHaarDetectObjects( img, cascade, storage,
+                                            1.1, 2, CV_HAAR_DO_CANNY_PRUNING,
+                                            cvSize(40, 40) );
+
+        // Loop the number of faces found.
+        for( i = 0; i < (faces ? faces->total : 0); i++ )
+        {
+           // Create a new rectangle for drawing the face
+            CvRect* r = (CvRect*)cvGetSeqElem( faces, i );
+
+            // Find the dimensions of the face,and scale it if necessary
+            pt1.x = r->x*scale;
+            pt2.x = (r->x+r->width)*scale;
+            pt1.y = r->y*scale;
+            pt2.y = (r->y+r->height)*scale;
+
+            // Draw the rectangle in the input image
+            cvRectangle( img, pt1, pt2, CV_RGB(255,0,0), 3, 8, 0 );
+        }
+    }
+
+    // Show the image in the window named "result"
+    //cvShowImage( "result", img );
+
+    // Release the temp image created.
+    cvReleaseImage( &temp );
+}*/
+
 
