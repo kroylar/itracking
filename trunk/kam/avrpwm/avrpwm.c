650c20212c2187a4a40ca2bff99c1df8ed3098d5
kroylar <kroylar@2674a16e-38bf-880a-777f-f6c340d2ef8c>
2010-11-06 18:09:39 +0000

    serialtest - program to test automated singals to the avr pan/tilt controller

--- trunk/kam/avrpwm/avrpwm.c
+++ trunk/kam/avrpwm/avrpwm.c
@@ -18,6 +18,8 @@
 
 #include "uart.h"
 
+//#define DEBUG
+
 uint8_t ch;
 
 static void ioinit(void) {
@@ -27,10 +29,10 @@ static void ioinit(void) {
     // fast PWM clk/8 
     TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11);
     TCCR1B = (1 << CS11) | (1 << WGM13) | (1 << WGM12);
-    OCR1A = 1499; // start at 1.5ms
-    OCR1A = 1499; // start at 1.5ms
+    OCR1A = 1300; // start at 1.5ms
+    OCR1B = 1300; // start at 1.5ms
 
-    ICR1 = 19999; // 20ms
+    ICR1 = 20000; // 20ms
 
     uart_init();
 }
@@ -45,26 +47,40 @@ int main(void) {
 
     stdout = stdin = stderr = &uart_str;
 
+#ifdef DEBUG
     fprintf(stderr, "Enter a new value for OCR1A and OCR1B to control the pulsewidth.\n");
+#endif
 
     while(1) {
+#ifdef DEBUG
         fprintf(stdout, "OCR1A = ?\n");
+#endif
         if (fgets(strbuf, sizeof strbuf - 1, stdin) == NULL) break;
         newval = atoi(strbuf);
         if (newval < 0 || newval > 19999) {
+#ifdef DEBUG
             fprintf(stdout, "Please enter a value between 0 and 19999.\n");
+#endif
         } else {
             OCR1A = newval;
+#ifdef DEBUG
             fprintf(stdout, "wrote 0x%x to OCR1A\n", newval);
+#endif
         }
+#ifdef DEBUG
         fprintf(stdout, "OCR1B = ?\n");
+#endif
         if (fgets(strbuf, sizeof strbuf - 1, stdin) == NULL) break;
         newval = atoi(strbuf);
         if (newval < 0 || newval > 19999) {
+#ifdef DEBUG
             fprintf(stdout, "Please enter a value between 0 and 19999.\n");
+#endif
         } else {
             OCR1B = newval;
+#ifdef DEBUG
             fprintf(stdout, "wrote 0x%x to OCR1B\n", newval);
+#endif
         }
     }
 
---
+++ trunk/kam/serialtest.c
@@ -0,0 +1,158 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+
+/* baudrate settings are defined in <asm/termbits.h>, which is
+included by <termios.h> */
+#define BAUDRATE B9600            
+/* change this definition for the correct port */
+#define MODEMDEVICE "/dev/ttyUSB5"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE; 
+struct termios oldtio;
+int fd;
+
+void ex_program(int sig);
+
+main()
+{
+  (void) signal(SIGINT, ex_program);
+  char tmp[10];
+  int c, res;
+  struct termios newtio;
+  char buf[255];
+  int OCR1A = 1560, OCR1B = 1520;
+/* 
+  Open modem device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
+*/
+ fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY ); 
+ if (fd <0) {perror(MODEMDEVICE); exit(-1); }
+
+ tcgetattr(fd,&oldtio); /* save current serial port settings */
+ memset(&newtio, 0, sizeof(newtio)); /* clear struct for new port settings */
+
+/* 
+  BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
+  CRTSCTS : output hardware flow control (only used if the cable has
+            all necessary lines. See sect. 7 of Serial-HOWTO)
+  CS8     : 8n1 (8bit,no parity,1 stopbit)
+  CLOCAL  : local connection, no modem contol
+  CREAD   : enable receiving characters
+*/
+ newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | HUPCL | CREAD;
+ 
+/*
+  IGNPAR  : ignore bytes with parity errors
+  ICRNL   : map CR to NL (otherwise a CR input on the other computer
+            will not terminate input)
+  otherwise make device raw (no other input processing)
+*/
+ newtio.c_iflag = IGNBRK;
+ 
+/*
+ Raw output.
+*/
+ newtio.c_oflag = 0;
+ 
+/*
+  ICANON  : enable canonical input
+  disable all echo functionality, and don't send signals to calling program
+*/
+ newtio.c_lflag = 0;
+ 
+/* 
+  initialize all control characters 
+  default values can be found in /usr/include/termios.h, and are given
+  in the comments, but we don't need them here
+*/
+ newtio.c_cc[VINTR]    = 0;     /* Ctrl-c */ 
+ newtio.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
+ newtio.c_cc[VERASE]   = 0;     /* del */
+ newtio.c_cc[VKILL]    = 0;     /* @ */
+ newtio.c_cc[VEOF]     = 4;     /* Ctrl-d */
+ newtio.c_cc[VTIME]    = 5;     /* inter-character timer unused */
+ newtio.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
+ newtio.c_cc[VSWTC]    = 0;     /* '\0' */
+ newtio.c_cc[VSTART]   = 0;     /* Ctrl-q */ 
+ newtio.c_cc[VSTOP]    = 0;     /* Ctrl-s */
+ newtio.c_cc[VSUSP]    = 0;     /* Ctrl-z */
+ newtio.c_cc[VEOL]     = 0;     /* '\0' */
+ newtio.c_cc[VREPRINT] = 0;     /* Ctrl-r */
+ newtio.c_cc[VDISCARD] = 0;     /* Ctrl-u */
+ newtio.c_cc[VWERASE]  = 0;     /* Ctrl-w */
+ newtio.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
+ newtio.c_cc[VEOL2]    = 0;     /* '\0' */
+ 
+ newtio.c_ispeed = 0xd;
+ newtio.c_ospeed = 0xd;
+
+/* 
+  now clean the modem line and activate the settings for the port
+*/
+ tcflush(fd, TCIFLUSH);
+ tcsetattr(fd,TCSANOW,&newtio);
+
+/*
+  terminal settings done, now handle input
+  In this example, inputting a 'z' at the beginning of a line will 
+  exit the program.
+*/
+#define DELAY 50000
+
+ while (STOP==FALSE) {     /* loop until we have a terminating condition */
+    sleep(1);
+     for (OCR1B = 1520; OCR1B >= 1280; OCR1B = OCR1B -= 10) {
+         sprintf(tmp,"%d\n",OCR1A);
+         write(fd,tmp,5);
+         usleep(DELAY);
+         sprintf(tmp,"%d\n",OCR1B);
+         write(fd,tmp,5);
+         usleep(DELAY);
+     }
+     sleep(1);
+     for (OCR1A = 1560; OCR1A >= 1280; OCR1A -= 10) {
+         sprintf(tmp,"%d\n",OCR1A);
+         write(fd,tmp,5);
+         usleep(DELAY);
+         sprintf(tmp,"%d\n",OCR1B);
+         write(fd,tmp,5);
+         usleep(DELAY);
+     }
+     sleep(1);
+     for (OCR1B = 1280; OCR1B <= 1520; OCR1B += 10) {
+         sprintf(tmp,"%d\n",OCR1A);
+         write(fd,tmp,5);
+         usleep(DELAY);
+         sprintf(tmp,"%d\n",OCR1B);
+         write(fd,tmp,5);
+         usleep(DELAY);
+     }
+     sleep(1);
+     for (OCR1A = 1280; OCR1A <= 1560; OCR1A += 10) {
+         sprintf(tmp,"%d\n",OCR1A);
+         write(fd,tmp,5);
+         usleep(DELAY);
+         sprintf(tmp,"%d\n",OCR1B);
+         write(fd,tmp,5);
+         usleep(DELAY);
+     }
+ }
+ /* restore the old port settings */
+ tcsetattr(fd,TCSANOW,&oldtio);
+}
+
+
+void ex_program(int sig) {
+    tcsetattr(fd,TCSANOW,&oldtio);
+    exit(0);
+}
